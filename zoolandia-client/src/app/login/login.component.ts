0d56fb2800107aa8dc2dd58e584faaba53a6b687
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2024-11-11 21:26:40 +0200

    Add login form and validators

--- zoolandia-client/src/app/login/login.component.ts
+++ zoolandia-client/src/app/login/login.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {confirmPasswordValidator} from "../confirm-password.validator";
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder
+  )
+  { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      'email': ['', [Validators.required, Validators.email]],
+      'password': ['', [Validators.required]]
+    })
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+  login() {
+    console.log(this.loginForm.value);
+  }
 }
