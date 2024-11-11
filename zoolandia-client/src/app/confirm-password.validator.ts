efc9df8478d26798cc683f9f9e6e1433473383af
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2024-11-11 21:26:17 +0200

    Add register form and validators

---
+++ zoolandia-client/src/app/confirm-password.validator.ts
@@ -0,0 +1,14 @@
+import {
+  AbstractControl,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
+
+export const confirmPasswordValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  const password = control.get('password')?.value;
+  const confirmPassword = control.get('confirmPassword')?.value;
+
+  return password === confirmPassword ? null : { PasswordNoMatch: true };
+};
---
+++ zoolandia-client/src/app/register/register.component.ts
@@ -0,0 +1,56 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+import { confirmPasswordValidator } from '../confirm-password.validator';
+
+
+@Component({
+  selector: 'register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit{
+  registerForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  )
+  { }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      'firstName': ['', [Validators.required]],
+      'lastName': ['', [Validators.required]],
+      'email': ['', [Validators.required, Validators.email, Validators.minLength(3), Validators.maxLength(50)]],
+      'phoneNumber': ['', [Validators.required, Validators.pattern('^[+]?[0-9]{9,15}$')]],
+      'password': ['', [Validators.required]],
+      'confirmPassword': ['', [Validators.required]]
+    }, { validators: confirmPasswordValidator })
+  }
+
+  get firstName() {
+    return this.registerForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.registerForm.get('lastName');
+  }
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get phoneNumber() {
+    return this.registerForm.get('phoneNumber');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get passwordMatchError() {
+    return this.registerForm.errors?.['PasswordNoMatch'] && this.registerForm.get('confirmPassword')?.touched;
+  }
+  register() {
+    console.log(this.registerForm.value);
+  }
+}
