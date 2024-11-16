5dc64ed746c04d7f84b3c1e5f26cbc35387b094a
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2024-11-16 10:36:32 +0200

    Client authentication finished

---
+++ zoolandia-client/src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private registerPath = environment.apiUrl + '/identity/register';
+  private loginPath = environment.apiUrl + '/identity/login';
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  register(data: any):Observable<any> {
+    return this.http.post(this.registerPath ,data);
+  }
+
+  login(data: any):Observable<any> {
+    return this.http.post(this.loginPath, data);
+  }
+
+  saveToken(token: any) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+}
