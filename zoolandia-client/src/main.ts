07d638966fe6e03cac1d9be18e40ed3cea24f192
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2024-12-25 19:32:41 +0200

    Add toastr service

--- zoolandia-client/src/main.ts
+++ zoolandia-client/src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {Routes} from '@angular/router';
 
-const routes: Routes = [];
-
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, appConfig, )
   .catch((err) => console.error(err));
