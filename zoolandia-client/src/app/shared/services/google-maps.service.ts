8d3a201c3c4ff333f8aece2ba97f261cfc0cb4e8
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2025-02-01 20:49:38 +0200

    Add Google Maps Service

---
+++ zoolandia-client/src/app/shared/services/google-maps.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleMapsService {
+  private apiLoaded = false;
+
+  constructor() {
+    if (typeof google !== 'undefined' && google.maps) {
+      this.apiLoaded = true;
+    }
+  }
+
+  loadGoogleMaps(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (this.apiLoaded) {
+        resolve();
+        return;
+      }
+
+      const existingScript = document.querySelector(`script[src*="maps.googleapis.com"]`);
+      if (existingScript) {
+        existingScript.addEventListener('load', () => {
+          this.apiLoaded = true;
+          resolve();
+        });
+        return;
+      }
+
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBtnT3myHIQx-EwUs6cDSIXnNkhNRdJpU4&libraries=places`;
+      script.async = true;
+      script.defer = true;
+
+      script.onload = () => {
+        this.apiLoaded = true;
+        resolve();
+      };
+      script.onerror = (error) => reject(error);
+
+      document.head.appendChild(script);
+    });
+  }
+}
