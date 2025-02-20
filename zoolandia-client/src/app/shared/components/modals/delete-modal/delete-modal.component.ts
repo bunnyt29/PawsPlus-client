512b3058b2b0f038bf9a3846c2e939dc561cd88f
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2025-02-20 13:24:04 +0200

    Add delete modal

---
+++ zoolandia-client/src/app/shared/components/modals/delete-modal/delete-modal.component.ts
@@ -0,0 +1,42 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ModalConfig} from '../../../models/ModalConfig';
+import {NgIf} from '@angular/common';
+import {PetService} from '../../../../pages/pet/services/pet.service';
+import {PostServiceService} from '../../../../pages/post/post-services/services/post-service.service';
+import {ToastrService} from 'ngx-toastr';
+import {Router} from '@angular/router';
+import {ModalService} from '../../../services/modal.service';
+
+@Component({
+  selector: 'app-delete-modal',
+  standalone: true,
+  imports: [
+    NgIf
+  ],
+  templateUrl: './delete-modal.component.html',
+  styleUrl: './delete-modal.component.scss'
+})
+export class DeleteModalComponent {
+  @Input() config!: ModalConfig;
+  @Output() closeModal = new EventEmitter<void>();
+
+  constructor(
+    private petService: PetService,
+    private postServiceService: PostServiceService,
+    private modalService: ModalService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {
+  }
+  deletePet(petId: string) {
+      this.petService.delete(petId).subscribe( () => {
+        this.toastr.success("Успешно изтри домашния си любимец!")
+        this.router.navigate(['/profile/my-profile-details/my-pets']);
+      });
+    }
+    deleteService(serviceId: string) {
+      this.postServiceService.delete(serviceId).subscribe(() => {
+        this.toastr.success("Успешно изтрита услуга!");
+      });
+    }
+}
