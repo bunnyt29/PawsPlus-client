73d5c82edf502c6ce00d8016622dfe0e49bc6705
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2025-02-20 13:25:54 +0200

    Add wrapper modal

---
+++ zoolandia-client/src/app/shared/components/modals/wrapper-modal/wrapper-modal.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {DetailsModalComponent} from '../details-modal/details-modal.component';
+import {DeleteModalComponent} from '../delete-modal/delete-modal.component';
+import {EditModalComponent} from '../edit-modal/edit-modal.component';
+import {AddModalComponent} from '../add-modal/add-modal.component';
+import {BookingModalComponent} from '../booking-modal/booking-modal.component';
+import {ModalService} from '../../../services/modal.service';
+@Component({
+  selector: 'app-wrapper-modal',
+  standalone: true,
+  imports: [
+    CommonModule,
+    DetailsModalComponent,
+    DeleteModalComponent,
+    EditModalComponent,
+    AddModalComponent,
+    BookingModalComponent
+  ],
+  templateUrl: './wrapper-modal.component.html',
+  styleUrls: ['./wrapper-modal.component.scss']
+})
+export class WrapperModalComponent {
+  constructor(public modalService: ModalService) {}
+
+  // Expose config and isOpen from the service using getters
+  get config() {
+    return this.modalService.config;
+  }
+
+  get isOpen() {
+    return this.modalService.isOpen;
+  }
+
+  // Called when a child modal requests to close
+  closeModal(): void {
+    this.modalService.close();
+  }
+}
