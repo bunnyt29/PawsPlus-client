59dfd3c3d0e14f503edfa14825b1f1fbb3810411
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2025-02-20 13:27:01 +0200

    Add modal config

---
+++ zoolandia-client/src/app/shared/models/ModalConfig.ts
@@ -0,0 +1,35 @@
+export class ModalConfig {
+  title?: string = '';
+  description?: string = '';
+  // A callback that receives form data when saving
+  save?: (data: any) => void = () => {};
+  // A callback for when the modal is cancelled/discarded
+  discard?: () => void = () => {};
+  // Any additional data you want to pass to the modal
+  data?: any = null;
+  type?: string = '';
+  // Action determines which modal to show: 'delete', 'edit', 'add', 'details', or 'book'
+  action?: 'delete' | 'edit' | 'add' | 'details' | 'book' = 'add';
+
+  constructor(
+    title: string = '',
+    description: string = '',
+    save?: (data: any) => void,
+    discard?: () => void,
+    data?: any,
+    type: string = '',
+    action: 'delete' | 'edit' | 'add' | 'details' | 'book' = 'add'
+  ) {
+    this.title = title;
+    this.description = description;
+    if (save) {
+      this.save = save;
+    }
+    if (discard) {
+      this.discard = discard;
+    }
+    this.data = data;
+    this.type = type;
+    this.action = action;
+  }
+}
